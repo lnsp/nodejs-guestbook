cf637d8b4d2c408862666fe9d65f0f75b0e505a5
Lennart Espe <lnsp@users.noreply.github.com>
2017-10-06 19:39:12 +0200

    Add eslint style guide
    Use eslint standard style guide
    
    Update package.json

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 // load express framework
-var bodyParser = require("body-parser");
-var express = require("express");
+var bodyParser = require('body-parser')
+var express = require('express')
+var urlEncoding = bodyParser.urlencoded({ extended: false })
 var app = express()
 
-var guestbook = [{
-    title: "My dear Frodo",
-    text: "You asked me once if I had told you everything there was to know about my adventures. And while I can honestly say I have told you the truth, I may not have told you all of it. I am old now, Frodo. I'm not the same hobbit as I once was. I think it is time for you to know what really happened.",
-    name: "Bilbo Baggins",
-}];
+var submissions = [{
+  text: 'My dear Frodo, you asked me once if I had told you everything there was to know about my adventures. And while I can honestly say I have told you the truth, I may not have told you all of it. I am old now, Frodo. I\'m not the same hobbit as I once was. I think it is time for you to know what really happened.',
+  name: 'Bilbo Baggins'
+}]
 
 // set render engine
-app.set("view engine", "pug")
+app.set('view engine', 'pug')
 
-var urlEncoding = bodyParser.urlencoded({ extended: false })
 // handle post submission
-app.post("/submit", urlEncoding, function(req, res) {
-    var entry = {
-        title: req.body.title,
-        text: req.body.text,
-        name: req.body.name,
-    };
-    console.log("submitted guestbook entry %s", entry);
-    guestbook.push(entry);
-    res.redirect("/");
+app.post('/submit', urlEncoding, function (req, res) {
+  var entry = {
+    text: req.body.text,
+    name: req.body.name
+  }
+  console.log('submitted guestbook entry %s', entry)
+  submissions.push(entry)
+  res.redirect('/')
 })
 
 // handle guestbook listing
-app.get("/", function(req, res) {
-    res.render("index", { title: "Guestbook", submissions: guestbook });
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Guestbook', submissions: submissions })
 })
 
-var server = app.listen(process.env.PORT, function() {
-    var host = server.address().address
-    var port = server.address().port
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address
+  var port = server.address().port
 
-    console.log("server running on http://%s:%s", host, port)
+  console.log('Server listening on http://%s:%s', host, port)
 })
